cc60ed193a0b8cdd2317e2aa74297a8f14e687b9
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-10-04 22:06:54 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int no = scanner.nextInt();
+        if (no % 2 != 0) {
+            System.out.println("Weird");
+        } else if (no % 2 == 0 && no >= 2 && no <= 5) {
+            System.out.println("Not Weird");
+        } else if (no % 2 == 0 && no >= 6 && no <= 20) {
+            System.out.println("Weird");
+        } else if (no % 2 == 0 && no > 20) {
+            System.out.println("Not Weird");
+        }
+
+        scanner.close();
+    }
+}
